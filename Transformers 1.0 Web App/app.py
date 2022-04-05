c40ae916ded4e980774e9680181d86adebae52fe
Linggar Maretva Cendani <linggarmc@gmail.com>
2022-04-05 09:07:52 +0700

    Update Dahsboard & AES, Finish Logic AES

--- Transformers 1.0 Web App/app.py
+++ Transformers 1.0 Web App/app.py
@@ -8,7 +8,25 @@ def index():
 
 @app.route('/aes', methods=['GET', 'POST'])
 def aes():
+	if request.method == 'POST':
+		
+		# Save Pertanyaan
+		pertanyaan = request.form['pertanyaan']
+		open('static/files/data/pertanyaan.txt', "w").write(pertanyaan)
+
+		# Save Jawaban
+		f = request.files['file']
+		f.save('static/files/data/jawaban.csv')
+
 	return render_template("aes.html")
+
+@app.route('/tutorial', methods=['GET', 'POST'])
+def tutorial():
+	return render_template("tutorial.html")
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+	return render_template("about.html")
 
 if __name__ == "__main__":
 	app.static_folder = 'static'
